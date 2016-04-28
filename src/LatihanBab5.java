bcec91bf83734a4274038ac17f69c7c15e1f040a
Nofry Arman <Nofry Arman>
2016-04-27 20:59:16 -0700

    isi data diri. class hitung.

--- src/LatihanBab5.java
+++ src/LatihanBab5.java
@@ -1,15 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
+import java.util.Scanner;
+class hitung{ 
+       public static void Penjumlahan(int bil1, int bil2){
+        int nilai = bil1 + bil2;
+        System.out.println("Hasil Penjumlahan : "+nilai);
+    }
+    public static void Pengurangan(int bil1, int bil2){
+        int nilai = bil1 - bil2;
+        System.out.println("Hasil Pengurangan : "+nilai);
+    }
+    public void Perkalian(int bil1, int bil2){
+        int nilai = bil1 * bil2;
+        System.out.println("Hasil Perkalian : "+nilai);
+    }
+    public void Pembagian(int bil1, int bil2){
+        double nilai = (double) bil1 / bil2;
+        System.out.println("Hasil Pembagian : "+nilai);
+    }
+    public void sederhana(int a, int b){
+        int pembilang = a, penyebut = b;
+        int temp,A,B;
+        if(penyebut == 0){
+            return;
+        }
+        A = (pembilang < penyebut)? penyebut : pembilang;
+        B = (pembilang < penyebut)?pembilang : penyebut;
+        while (B != 0){
+            temp = A % B;
+            A = B;
+            B = temp;
+        
+        }
+        pembilang /= A;
+        penyebut /=A;
+        System.out.println(pembilang+"/"+penyebut);
+    }
 public class LatihanBab1 {
     public static void main(String[] args) {
-        System.out.println("tess");
         
         
     }
